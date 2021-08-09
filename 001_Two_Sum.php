d07c72f317be58f125ec8bb80e83944a412620f4
Ashraful Alam <ashraful.alam@o9solutions.com>
2021-08-09 17:07:42 +0900

    001_two_sum_hashmap

--- 001_Two_Sum.php
+++ 001_Two_Sum.php
@@ -1,24 +1,49 @@
-  class Solution {
+<?php
+class Solution
+{
     /**
-    * @param Integer[] $nums
-    * @param Integer $target
-    * @return Integer[]
-    */
-    //Brute Force
-    function twoSum($nums, $target) {
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+
+    //hash map solution (runtime 20ms; complexity o(n))
+    function twoSum($nums, $target)
+    {
+        $output = [];
+        $hashMap = [];
+        foreach ($nums as $hashKey => $hashNum) {
+            $hashMap[$hashNum] = $hashKey;
+        }
+
+        foreach ($nums as $key => $num) {
+            $diff = $target - $num;
+            if ($hashMap[$diff] && $key !== $hashMap[$diff]) {
+                $output[] = $key;
+                $output[] = $hashMap[$diff];
+                return $output;
+            }
+        }
+    }
+
+    //Brute Force solution (runtime 2404ms; complexity o(n2))
+    /*
+    function twoSum($nums, $target)
+    {
         $output = [];
         $copy_nums = $nums;
-        foreach($nums as $key1=>$num){
+        foreach ($nums as $key1 => $num) {
             unset($copy_nums[$key1]);
             $temp_nums = $copy_nums;
-            foreach($temp_nums as $key2=>$temp_num){
-                if($num+$temp_num == $target){
-                    $output[]=$key1;
-                    $output[]=$key2;
+            foreach ($temp_nums as $key2 => $temp_num) {
+                if ($num + $temp_num == $target) {
+                    $output[] = $key1;
+                    $output[] = $key2;
                     return $output;
                 }
             }
             $copy_nums = $nums;
         }
     }
+    */
 }
